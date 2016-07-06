f2b4cebb38e569b97aed2c43800bf96c147105b5
breadbrad <yimdonghoon@gmail.com>
2016-07-05 20:06:02 -0500

    added something

---
+++ recursive/recursive_examples.java
@@ -0,0 +1,25 @@
+/** Recursive algorithm */
+public static int length (String str){
+	if (str == null || str.equals(""))
+		return 0;
+	else 
+		return 1 + length(str.substring(1));
+}
+
+public static void printChars(String str) {
+	if (str == null || str.equals("")) {
+		return;
+	} else {
+		System.out.println(str.charAt(0));
+		printChars(str.substring(1));
+	}
+}
+
+public static void printCharsReverse(String str) {
+	if (str == null || str.equals("")) {
+		return;
+	} else {
+		printCharsReverse(str.substring(1));
+		System.out.print(str.charAt(0));
+	}
+}
