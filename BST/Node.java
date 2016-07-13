51c046048e28ae3e9cdf558b88464bf3a49791af
breadbrad <yimdonghoon@gmail.com>
2016-07-12 23:51:55 -0500

    added commentary

--- BST/Node.java
+++ BST/Node.java
@@ -1,9 +1,13 @@
 /** private -> only be accessed within the declared class itself
-	protected -> can be accessed by the subclasses in other package */
+	protected -> can be accessed by the subclasses in other package 
+	class Node<T> -> is nested within class BinaryTree<T>. 
+	we will use the BinaryTree<T> and Node<T> classes as superclasses 
+	Serializable interface -> defines no methods, used to provide a marker for classes 
+	(can be written to a binary file using the ObjectOutputStream / read by ObjectInputStream*/
 
 
 
-public static class Node<T> implements Serializable {
+protected static class Node<T> implements Serializable {
 	protected T data;
 
 	protected Node<T> left;
