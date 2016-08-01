78cd00d83d174d03022949dfa3d8d1bffb5a3ebb
breadbrad <yimdonghoon@gmail.com>
2016-07-31 20:23:53 -0500

    Organize..

---
+++ Sorting/merge_sort.py
@@ -0,0 +1,31 @@
+def merge_sort(A):
+    """
+    Sort list A into order, and return result.
+    """
+    n = len(A)
+    if n==1: 
+        return A
+    mid = n//2     # floor division
+    L = merge_sort(A[:mid])
+    R = merge_sort(A[mid:])
+    return merge(L,R)
+
+def merge(L,R):
+    """
+    Given two sorted sequences L and R, return their merge.
+    """
+    i = 0
+    j = 0
+    answer = []
+    while i<len(L) and j<len(R):
+        if L[i]<R[j]:
+            answer.append(L[i])
+            i += 1
+        else:
+            answer.append(R[j])
+            j += 1
+    if i<len(L):
+        answer.extend(L[i:])
+    if j<len(R):
+        answer.extend(R[j:])
+    return answer
