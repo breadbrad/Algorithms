743628d1f2e1ea45fe1de392918450adab0f4dce
breadbrad <yimdonghoon@gmail.com>
2016-08-30 19:55:32 -0500

    Fix typoes

--- java_examples/SortLargeFile.java
+++ java_examples/SortLargeFile.java
@@ -4,7 +4,7 @@ class SortLageFile {
         public static final int MAX_ARRAY_SIZE = 100000;
         public static final int BUFFER_SIZE = 100000;
 
-        public static void main(String[] args) {
+        public static void main(String[] args) throws Exception {
                 sort("largedata.dat", "sortedfile.dat");
 
                 displayFile("sortedfile.dat");
@@ -13,13 +13,14 @@ class SortLageFile {
         /** Sort data in source file and into target file */
         public static void sort(String sourceFile, String targetFile) throws Exception {
                /** Implement Phase 1 : Create initial segments */  
-               int numberofSegments = initializeSegments (MAX_ARRAY_SIZE, sourcefile, "f1.dat");
+               int numberofSegments = initializeSegments (MAX_ARRAY_SIZE, sourceFile, "f1.dat");
                /** Implement Phase 2 : Merge segment recursively */        
-               merge(numberofSegments, MAX_ARRAY_SIZE, "f1.dat", "f2.dat", "f3.dat", targetfile);
+               merge(numberofSegments, MAX_ARRAY_SIZE, "f1.dat", "f2.dat", "f3.dat", targetFile);
         }
 
         
-        public static int initializeSegment(int segmentSize, String originalFile, String f1) throws Exception {
+        public static int initializeSegments(int segmentSize, String originalFile, String f1) throws Exception {
+        int[] list = new int[segmentSize];
                 DataInputStream input = new DataInputStream(new BufferedInputStream (new FileInputStream(originalFile)));
                 DataOutputStream output= new DataOutputStream(new BufferedOutputStream(new FileOutputStream(f1)));
                 
@@ -29,7 +30,7 @@ class SortLageFile {
                        for (; i < input.available() && i < segmentSize; i++) 
                                  list[i] = input.readInt();
                         // sort the array list[0 .. i-1]
-                       java.util.Array(list, 0, i);
+                       java.util.Arrays.sort(list, 0, i);
 
                        // Write the array to f1.dat
                         for (int j = 0; j < i; j++) 
@@ -44,27 +45,26 @@ class SortLageFile {
         }
 
        private static void merge(int numberOfSegments, int segmentSize, String f1, String f2, String f3, String targetfile) throws Exception {
-                if( numberofSegments > 1) {
-                        mergeOneStep(numberofSegments, segmentSize, f1, f2, f3);
-                        merge((numberofSegments + 1) / 2, segmentSize * 2, f3, f1, f2, targetFile);
+                if( numberOfSegments > 1) {
+                        mergeOneStep(numberOfSegments, segmentSize, f1, f2, f3);
+                        merge((numberOfSegments + 1) / 2, segmentSize * 2, f3, f1, f2, targetfile);
                 }
                 else { // Rename f1 as the final sorted file 
-                        File sortedFile = new File (targetfile);
-                        if (sortedFile.exist()) sortedFile.delete();
-                        new File(f1).renameTo(sortedFile);
+                File sortedFile = new File(targetfile);
+                if (sortedFile.exists()) sortedFile.delete(); new File(f1).renameTo(sortedFile);
                 }
 }
 
         private static void mergeOneStep(int numberofSegments, int segmentSize, String f1, String f2, String f3) throws Exception {
                 DataInputStream f1Input = new DataInputStream(new BufferedInputStream(new FileInputStream(f1), BUFFER_SIZE));
-                DataOutputStream f2Output = new DataOutputStream(new BufferedInputStream(new FileOutputStream(f2), BUFFER_SIZE));
+                DataOutputStream f2Output = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(f2), BUFFER_SIZE));
 
                 copyHalfToF2(numberofSegments, segmentSize, f1Input, f2Output);
                 f2Output.close();
 
                 // Merge remaining segments in f1 with segments in f2 into f3
                 DataInputStream f2Input = new DataInputStream(new BufferedInputStream(new FileInputStream(f2), BUFFER_SIZE));
-                DataOutputStream f3Output = new DataOutputStream(new BufferedInputStream(new FileOutputStream(f3), BUFFER_SIZE));
+                DataOutputStream f3Output = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(f3), BUFFER_SIZE));
                 mergeSegments(numberofSegments / 2, segmentSize, f1Input, f2Input, f3Output);
 
                 f1Input.close();
@@ -73,13 +73,13 @@ class SortLageFile {
         }
        
         
-        private static void copyHalfToF2 (int numberOfSegment, int segmentSize, DataInputStream f1, DataOutputStream f2) throws Exception {
+        private static void copyHalfToF2 (int numberOfSegments, int segmentSize, DataInputStream f1, DataOutputStream f2) throws Exception {
                 for (int i = 0; i < (numberOfSegments / 2) * segmentSize; i++)
                         f2.writeInt(f1.readInt());
         }
 
 
-        private static void mergeSegments (int numberOfSegment, int segmentSize, DataInputStream f1, DataInputStream f2, DataOutputStream f3) throws Exception {
+        private static void mergeSegments (int numberOfSegments, int segmentSize, DataInputStream f1, DataInputStream f2, DataOutputStream f3) throws Exception {
                 for (int i = 0; i < numberOfSegments; i++) 
                         mergeTwoSegments(segmentSize, f1, f2, f3);
                         
@@ -88,7 +88,7 @@ class SortLageFile {
 
         }
 
-        private static void mergeTwoSegments(int segmentSize, DataInputStream f1, DataInputStream f2, DataOuputStream f3) throws Exception {
+        private static void mergeTwoSegments(int segmentSize, DataInputStream f1, DataInputStream f2, DataOutputStream f3) throws Exception {
                 int intFromF1 = f1.readInt();
                 int intFromF2 = f2.readInt();
                 int f1Count = 1;
@@ -97,7 +97,7 @@ class SortLageFile {
                 while(true) {
                         if(intFromF1 < intFromF2) {      
                                 f3.writeInt(intFromF1);
-                                if (f1.available() ==  0 && || f1Count++ >= segmentSize) {
+                                if (f1.available() ==  0  || f1Count++ >= segmentSize) {
                                         f3.writeInt(intFromF2);
                                         break;
                                 }
@@ -107,12 +107,12 @@ class SortLageFile {
                         }
                         else { 
                                 f3.writeInt(intFromF2);
-                                if (f2.available() == 0 && || f2Count++ >= segmentSize) {
+                                if (f2.available() == 0  || f2Count++ >= segmentSize) {
                                         f3.writeInt(intFromF1);
                                         break;
                                 } 
                                 else { 
-                                        intfromF2 = f2.readInt();
+                                        intFromF2 = f2.readInt();
                                 }
                         }
                 }
